6f5ca4dd515ca015cfca391b48ade39198028c79
AndreeaZoicas <zoicas.andree@yahoo.com>
2024-11-11 22:59:01 +0200

    Laborator5

--- Data/Andreea_Zoicas_Lab2Context.cs
+++ Data/Andreea_Zoicas_Lab2Context.cs
@@ -18,6 +18,8 @@ namespace Andreea_Zoicas_Lab2.Data
         public DbSet<Andreea_Zoicas_Lab2.Models.Publisher> Publisher { get; set; } = default!;
         public DbSet<Andreea_Zoicas_Lab2.Models.Author> Author { get; set; } = default!;
         public DbSet<Andreea_Zoicas_Lab2.Models.Category> Category { get; set; } = default!;
+        public DbSet<Andreea_Zoicas_Lab2.Models.Member> Member { get; set; } = default!;
+        public DbSet<Andreea_Zoicas_Lab2.Models.Borrowing> Borrowing { get; set; } = default!;
 
     }
 }
---
+++ Pages/Borrowings/Index.cshtml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Andreea_Zoicas_Lab2.Data;
+using Andreea_Zoicas_Lab2.Models;
+
+namespace Andreea_Zoicas_Lab2.Pages.Borrowings
+{
+    public class IndexModel : PageModel
+    {
+        private readonly Andreea_Zoicas_Lab2.Data.Andreea_Zoicas_Lab2Context _context;
+
+        public IndexModel(Andreea_Zoicas_Lab2.Data.Andreea_Zoicas_Lab2Context context)
+        {
+            _context = context;
+        }
+
+        public IList<Borrowing> Borrowing { get; set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            if (_context.Borrowing != null)
+            {
+                Borrowing = await _context.Borrowing
+                .Include(b => b.Book)
+                .ThenInclude(b => b.Author)
+                .Include(b => b.Member).ToListAsync();
+            }
+        }
+    }
+}
